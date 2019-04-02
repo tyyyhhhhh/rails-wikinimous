02b2557e6012c1e01a80e1ca838f44d7109d01ad
yinghui tan <yinghuitan18@gmail.com>
2019-04-02 16:34:50 +0800

    using rails shortcuts

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,47 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new
+    @article.title = params[:article][:title]
+    @article.content = params[:article][:content]
+    @article.save!
+
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article = @article.update(article_params)
+
+    redirect_to article_path
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+private
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+end
